49d01935f51a46d6817394c59d96193692528c8f
Agnyathavasi <vishwajithhh12345@gmail.com>
2025-04-20 20:13:49 +0530

    Added search and loader states

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -4,23 +4,34 @@ import ProductCard from "./ProductCard";
 
 export default function Pagination({ api }) {
   const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
   const [page, setPages] = useState(1);
-  const [pagination, setPagination] = useState({ page: 1, pageSize: 15 });
+  const [pagination, setPagination] = useState({
+    page: 1,
+    pageSize: 15,
+    search: "",
+  });
   const [total, setTotal] = useState(0);
+  const [search, setSearch] = useState("");
 
   useEffect(() => {
     fetchProducts();
-  }, [pagination.page, pagination.pageSize]);
+  }, [pagination.page, pagination.pageSize, pagination.search]);
 
   async function fetchProducts() {
-    const result = await axios.get(api, {
+    setLoading(true);
+    let uuId = "";
+    if (pagination.search) uuId = "search";
+    const result = await axios.get(api + "/" + uuId, {
       params: {
         limit: pagination.pageSize,
         skip: (pagination.page - 1) * pagination.pageSize,
+        q: pagination.search,
       },
     });
     setTotal(result.data.total);
     setProducts(result.data.products);
+    setLoading(false);
   }
 
   function pageChange(updatePage) {
@@ -37,68 +48,94 @@ export default function Pagination({ api }) {
   function setPage(e) {
     if (
       e.target.value < 0 ||
-      e.target.value > Math.ceil(total / pagination.pageSize)
+      e.target.value >= Math.ceil(total / pagination.pageSize)
     )
       return;
     setPages(e.target.value);
   }
+
+  function setSearches(e) {
+    if (e.key === "Enter") {
+      setPages(1);
+      setPagination((prev) => ({ ...prev, search: search, page: 1 }));
+    }
+  }
   return (
     <>
-      <section className="products-page">
-        {products.length &&
-          products.map((item) => <ProductCard key={item.id} product={item} />)}
-      </section>
-      <section className="pagination">
-        <span
-          className={pagination.page === 1 ? "disabled" : ""}
-          style={{ cursor: "pointer" }}
-          onClick={() => pageChange(1)}
-        >
-          ⏮️
-        </span>
-        <span
-          className={pagination.page === 1 ? "disabled" : ""}
-          style={{ cursor: "pointer" }}
-          onClick={() => pageChange(pagination.page - 1)}
-        >
-          ◀️
-        </span>
+      <section
+        className="search"
+        style={{
+          display: "flex",
+          justifyContent: "flex-end",
+          padding: "10px 50px",
+        }}
+      >
         <input
-          value={page}
-          style={{ marginRight: "10px" }}
-          type="number"
-          min="1"
-          max={Math.ceil(total / pagination.pageSize)}
-          onChange={setPage}
-          onKeyDown={updatePage}
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+          placeholder="Search"
+          onKeyDown={setSearches}
         />
-        of
-        <div style={{ marginLeft: "10px" }}>
-          {Math.ceil(total / pagination.pageSize)}
-        </div>
-        <span
-          className={
-            pagination.page === Math.ceil(total / pagination.pageSize)
-              ? "disabled"
-              : ""
-          }
-          style={{ cursor: "pointer" }}
-          onClick={() => pageChange(pagination.page + 1)}
-        >
-          ▶️
-        </span>
-        <span
-          className={
-            pagination.page === Math.ceil(total / pagination.pageSize)
-              ? "disabled"
-              : ""
-          }
-          style={{ cursor: "pointer" }}
-          onClick={() => pageChange(Math.ceil(total / pagination.pageSize))}
-        >
-          ⏭️
-        </span>
       </section>
+      <section className="products-page">
+        {loading && <h2>Loading...</h2>}
+        {!loading && products.length
+          ? products.map((item) => <ProductCard key={item.id} product={item} />)
+          : !loading && <h3>No items found.</h3>}
+      </section>
+      {products.length > 0 && (
+        <section className="pagination">
+          <span
+            className={pagination.page === 1 ? "disabled" : ""}
+            style={{ cursor: "pointer" }}
+            onClick={() => pageChange(1)}
+          >
+            ⏮️
+          </span>
+          <span
+            className={pagination.page === 1 ? "disabled" : ""}
+            style={{ cursor: "pointer" }}
+            onClick={() => pageChange(pagination.page - 1)}
+          >
+            ◀️
+          </span>
+          <input
+            value={page}
+            style={{ marginRight: "10px" }}
+            type="number"
+            min="1"
+            max={Math.ceil(total / pagination.pageSize)}
+            onChange={setPage}
+            onKeyDown={updatePage}
+          />
+          of
+          <div style={{ marginLeft: "10px" }}>
+            {Math.ceil(total / pagination.pageSize)}
+          </div>
+          <span
+            className={
+              pagination.page === Math.ceil(total / pagination.pageSize)
+                ? "disabled"
+                : ""
+            }
+            style={{ cursor: "pointer" }}
+            onClick={() => pageChange(pagination.page + 1)}
+          >
+            ▶️
+          </span>
+          <span
+            className={
+              pagination.page === Math.ceil(total / pagination.pageSize)
+                ? "disabled"
+                : ""
+            }
+            style={{ cursor: "pointer" }}
+            onClick={() => pageChange(Math.ceil(total / pagination.pageSize))}
+          >
+            ⏭️
+          </span>
+        </section>
+      )}
     </>
   );
 }
--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -5,16 +5,33 @@ export default function ProductCard({ product }) {
     else return "red";
   }
 
+  function calculateOriginalPrice(discount, price) {
+    const n = parseFloat((price * discount) / 100 + price, 2);
+    return n.toFixed(2);
+  }
+
   return (
     <div className="product-item">
       <img
         src={product.thumbnail}
+        style={{ backgroundColor: "#eeeeee" }}
         className="product-image"
         alt={product.title}
       />
       <span className="product-title">
         <h4 className="mb-0">{product.title}</h4>
-        <h4 className="mb-0">${product.price}</h4>
+        <h4 className="mb-0">
+          <div>${product.price}</div>
+          <div
+            style={{
+              textDecoration: "line-through",
+              fontSize: "12px",
+              color: "#888",
+            }}
+          >
+            ${calculateOriginalPrice(product.discountPercentage, product.price)}
+          </div>
+        </h4>
       </span>
       <h5 className="mt-0">{product.brand}</h5>
       <div style={{ fontSize: "12px", marginBottom: "5px" }}>
